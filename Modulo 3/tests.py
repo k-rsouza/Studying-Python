5376e35c977edd935068e1c529951fc526cd15dd
k-rsouza <k.souza@edu.pucrs.br>
2025-08-19 00:26:39 -0300

    Adding tests & Updating create endpoint

--- Modulo 3/tests.py
+++ Modulo 3/tests.py
@@ -23,3 +23,43 @@ def test_get_tasks():
     response_json = response.json()
     assert "tasks" in response_json
     assert "total_tasks" in response_json
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']    # Entre colchetes pois esta acessando um valor associado a chave ID em um dicionario
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload =  {
+            "completed": False,
+            "description": "Nova descrição",
+            "title": "Novo Título"
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+        
+# Nova requisição a tarefa específica 
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["title"] == payload["title"]
+        assert response_json["description"] == payload["description"]
+        assert response_json["completed"] == payload["completed"]
+
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        response.status_code == 200
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404     # Se retornar ERROR 404 significa que a tarefa nao foi encontrada, logo foi deletada com sucesso
+                                               # e esse eh o comportamento esperado
