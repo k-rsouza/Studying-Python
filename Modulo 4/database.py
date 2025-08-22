4cdf8059ffa85f3ade119e2288e696b73f4dd2d4
k-rsouza <k.souza@edu.pucrs.br>
2025-08-22 02:21:59 -0300

    Separating Module 4 from this repository

--- Modulo 4/database.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-
-db = SQLAlchemy()   # criando instancia do ORM e passando a classe como parametro
-'''
-SQLAlchemy é iniciado como uma instância, ao rodar o flask shell e create all
-ele cria o banco de dados baseado em tudo que ele conseguiu enxergar pelo db model
-'''
